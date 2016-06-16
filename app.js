8ac2603ca7bae3c5243f40575ad25be123678ad7
Xiaocheng Xie <Xiaocheng@Xiaocheng-Xie.local>
2016-06-16 13:44:47 -0230

    First commit on Thursday

---
+++ app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// mysql test
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'xx',
+  database : 'mydb'
+});
+
+connection.connect();
+
+connection.query('SELECT * from Records', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].recordText);
+});
+
+connection.end();
